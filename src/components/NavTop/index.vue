08e5fee61a563630c105e4a6dbc69b8b58f7f30d
daizch <daizech@gmail.com>
2018-09-25 13:52:11 +0800

    update navtop

--- src/components/NavTop/index.vue
+++ src/components/NavTop/index.vue
@@ -3,17 +3,18 @@
     <div class="nav-top-container" v-if="userInfo">
       <ul class="nav-top-right">
         <li class="nav-top-item">
-          <el-dropdown @command="handleNavTopCommand">
-                      <span class="el-dropdown-link">
-                        {{userInfo.nickname}}<i class="el-icon-arrow-down el-icon--right"></i>
-                      </span>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item command="gotoAccountSetting">账户设置</el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
+          <!--{{userInfo.nickname}}-->
+          <!--<el-dropdown @command="handleNavTopCommand">-->
+                      <!--<span class="el-dropdown-link">-->
+                        <!--{{userInfo.nickname}}<i class="el-icon-arrow-down el-icon&#45;&#45;right"></i>-->
+                      <!--</span>-->
+            <!--<el-dropdown-menu slot="dropdown">-->
+              <!--<el-dropdown-item command="gotoAccountSetting">账户设置</el-dropdown-item>-->
+            <!--</el-dropdown-menu>-->
+          <!--</el-dropdown>-->
         </li>
         <li class="nav-top-item" @click="logoutHandler">退出</li>
-        <li class="nav-top-item"><a href="/">我的feth</a></li>
+        <li class="nav-top-item"><a href="/">我的freelog</a></li>
       </ul>
     </div>
   </div>
