6946dd5faa19cfc3ca45bbbe05969f257c89dd9e
wwzh <790727372@qq.com>
2018-10-15 10:29:52 +0800

    add freelog api test

--- test/freelog-core/api.test.js
+++ test/freelog-core/api.test.js
@@ -2,28 +2,46 @@ var expect = require('chai').expect
 const puppeteer = require('puppeteer')
 
 describe('Test API: window.Freelog.QI.***', async function (){
-  it('window.Freelog.QI.fetchPresentablesList is OK', async function (){
-    const browser = await puppeteer.launch({
+  var browser, page
+
+  before(async () => {
+    browser = await puppeteer.launch({
       executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
       headless: true
     })
 
-    const page = await browser.newPage()
+    page = await browser.newPage()
+    // page.on('console', msg => console.log(msg.text()))
 
-    page.on('console', msg => console.log(msg.text()))
-
-    page.once('load', async () => {
-      await page.evaluate(async () => {
-        var res = await window.FreelogApp.QI.fetchPresentablesList()
-      })
-      await new Promise((resolve) => {
-        expect(0).to.equal(1)
+    return new Promise(async (resolve) => {
+      page.once('load', async () => {
         resolve()
       })
-      await browser.close()
+      await page.goto('http://local.testfreelog.com')
     })
-
-    await page.goto('http://local.testfreelog.com')
   })
+
+  after(async () => {
+    await browser.close()
+  })
+
+  it('window.Freelog.QI.fetchPresentablesList is OK', async function (){
+    return new Promise(async (resolve, reject) => {
+      var pLResponse = await page.evaluate(async () => {
+        var res = await window.FreelogApp.QI.fetchPresentablesList()
+        return res
+      })
+
+      try{
+        expect(pLResponse, 'response is invalid!').to.be.an('Object').that.have.property('ret')
+        expect(pLResponse.ret).to.equal(0, `msg:${pLResponse.msg}`)
+      }catch(e){
+        return reject(e)
+      }
+
+      resolve()
+    })
+  })
+
 })
 
