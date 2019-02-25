d490076672d5af07a3974d63c3232a63e0adb46b
daizch <daizech@gmail.com>
2019-02-25 15:08:32 +0800

    注册&修改密码添加验证码验证

--- src/views/user/reset-password/index.vue
+++ src/views/user/reset-password/index.vue
@@ -11,15 +11,25 @@
       <el-form-item prop="loginName" :label="$t('resetPassword.loginName')">
         <el-input type="text" v-model="model.loginName" :placeholder="$t('resetPassword.loginNamePlaceholder')"></el-input>
       </el-form-item>
+      <el-form-item prop="authCode" :label="$t('resetPassword.verifyCode')">
+        <el-input type="text" v-model="model.authCode"
+                  style="width: 60%"
+                  :placeholder="$t('resetPassword.verifyCodeInputTip')"></el-input>
+        <el-button class="vcode-btn"
+                   @click="sendCheckCodeNotifyHandler"
+                   :disabled="disabledCheckCodeBtn">
+          {{vcodeBtnText}}
+        </el-button>
+      </el-form-item>
       <el-form-item prop="password" :label="$t('resetPassword.password')">
-        <el-input type="password" v-model="model.password" placeholder=""></el-input>
+        <el-input type="password" v-model="model.password"
+                  autocomplete="new-password"
+                  :readonly="readonly"
+                  :placeholder="$t('resetPassword.inputPasswordTip')"></el-input>
       </el-form-item>
       <el-form-item>
         <router-link to="/login" class="user-op">{{$t('resetPassword.backToLogin')}}<i class="el-icon-d-arrow-right"></i></router-link>
       </el-form-item>
-      <!--<el-form-item prop="verifyCode" label="验证码">-->
-      <!--<el-input type="text" v-model="model.verifyCode" placeholder=""></el-input>-->
-      <!--</el-form-item>-->
       <el-form-item class="login-btns">
         <el-button type="primary"
                    style="width: 100%;"
@@ -48,7 +58,7 @@ export default ResetView
     -moz-border-radius: 5px;
     background-clip: padding-box;
     margin: 180px auto;
-    width: 350px;
+    width: 400px;
     min-height: 300px;
     padding: 35px 35px 15px 35px;
     background: #fff;
@@ -73,6 +83,10 @@ export default ResetView
       &:hover {
         color: #409EFF;
       }
+    }
+
+    .vcode-btn {
+      margin-left: 10px;
     }
   }
 </style>
--- src/views/user/signup/index.vue
+++ src/views/user/signup/index.vue
@@ -11,21 +11,38 @@
     <el-form class="login-form" auto-complete="off" :model="model" :rules="rules" ref="signupForm" label-width="80px">
       <h2 class="heading">{{$t('signup.head')}}</h2>
       <el-form-item prop="loginName" :label="$t('signup.loginName')" required>
-        <el-input type="text" v-model="model.loginName" :placeholder="$t('signup.loginNamePlaceholder')">
+        <el-input type="text" v-model="model.loginName"
+                  :placeholder="$t('signup.loginNamePlaceholder')" autocomplete="off">
         </el-input>
       </el-form-item>
       <el-form-item prop="nickname" :label="$t('signup.nickname')" required>
-        <el-input type="text" v-model="model.nickname" :placeholder="$t('signup.nicknamePlaceholder')">
+        <el-input type="text" v-model="model.nickname"
+                  :placeholder="$t('signup.nicknamePlaceholder')" autocomplete="off">
         </el-input>
       </el-form-item>
       <el-form-item prop="password" :label="$t('signup.password')" required>
-        <el-input type="password" v-model="model.password" :placeholder="$t('signup.passwordPlaceholder')"></el-input>
+        <el-input type="password" v-model="model.password"
+                  :placeholder="$t('signup.passwordPlaceholder')"
+                  autocomplete="new-password"
+                  :readonly="readonly"></el-input>
       </el-form-item>
       <el-form-item prop="checkPassword" :label="$t('signup.checkPassword')" required>
-        <el-input type="password" v-model="model.checkPassword" :placeholder="$t('signup.checkPasswordPlaceholder')"></el-input>
+        <el-input type="password" v-model="model.checkPassword"
+                  :placeholder="$t('signup.checkPasswordPlaceholder')"
+                  autocomplete="new-password"
+                  :readonly="readonly"></el-input>
       </el-form-item>
-      <el-form-item prop="verifyCode" :label="$t('signup.verifyCode')" v-show="false">
-        <el-input v-model="model.verifyCode" :placeholder="$t('signup.verifyCodePlaceholder')"></el-input>
+      <el-form-item prop="authCode" :label="$t('signup.verifyCode')">
+        <el-input v-model="model.authCode"
+                  style="width: 60%"
+                  :placeholder="$t('signup.verifyCodePlaceholder')"></el-input>
+        <div class="check-code-wrap">
+          <el-button class="vcode-btn"
+                     @click="sendCheckCodeNotifyHandler"
+                     :disabled="disabledCheckCodeBtn">
+            {{vcodeBtnText}}
+          </el-button>
+        </div>
       </el-form-item>
       <el-form-item>
         <router-link to="/login" class="user-op">{{$t('signup.loginText')}}<i class="el-icon-d-arrow-right"></i></router-link>
