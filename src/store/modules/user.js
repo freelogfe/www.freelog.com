1fdfca72a291b8234e3ee6f6cc208c392bb9b0dc
kai <kai@kaideMacBook-Pro.local>
2019-06-18 11:09:27 +0800

    Update user.js

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,114 +1,114 @@
-import { storage, axios } from '@/lib/index'
-import { cookieStore } from '@/lib/storage'
+import {storage, axios} from '@/lib/index'
+import {cookieStore} from '@/lib/storage'
 
 const types = {
-  LOAD_CURRENT_USER: 'loadCurrentUserInfo',
-  GET_CURRENT_USER_INFO: 'getCurrentUserInfo',
-  CHANGE_SESSION: 'changeSession',
-  USER_LOGIN: 'userLogin',
-  CHECK_USER_SESSION: 'checkUserSession',
-  DELETE_USER_SESSION: 'deleteUserSession',
-  LOGOUT: 'userLogout',
-  CHANGE_AVATAR: 'changeAvatar'
+    LOAD_CURRENT_USER: 'loadCurrentUserInfo',
+    GET_CURRENT_USER_INFO: 'getCurrentUserInfo',
+    CHANGE_SESSION: 'changeSession',
+    USER_LOGIN: 'userLogin',
+    CHECK_USER_SESSION: 'checkUserSession',
+    DELETE_USER_SESSION: 'deleteUserSession',
+    LOGOUT: 'userLogout',
+    CHANGE_AVATAR: 'changeAvatar'
 }
 
 
 function resolveAvatarUrl(userId) {
-  return `https://image.freelog.com/headImage/${userId}?x-oss-process=style/head-image`
+    return `https://image.freelog.com/headImage/${userId}?x-oss-process=style/head-image`
 }
 
 const user = {
-  state: {
-    session: null // storage.get('user_session')
-  },
+    state: {
+        session: null // storage.get('user_session')
+    },
 
-  mutations: {
-    [types.CHANGE_SESSION](state, data) {
-      data.avatarUrl = resolveAvatarUrl(data.userId)
-      state.session = data
-      storage.set('user_session', state.session)
+    mutations: {
+        [types.CHANGE_SESSION](state, data) {
+            data.avatarUrl = resolveAvatarUrl(data.userId)
+            state.session = data
+            storage.set('user_session', state.session)
+        },
+        [types.CHANGE_AVATAR](state, data) {
+            if (typeof data === 'undefined') {
+                state.session.avatarUrl = `${resolveAvatarUrl(state.session.userId)}&_c=${+new Date()}`
+            } else {
+                state.session.avatarUrl = data
+            }
+        },
+        [types.LOGOUT](state) {
+            state.session = null
+            storage.remove('user_session')
+        }
     },
-    [types.CHANGE_AVATAR](state, data) {
-      if (typeof data === 'undefined') {
-        state.session.avatarUrl = `${resolveAvatarUrl(state.session.userId)}&_c=${+new Date()}`
-      } else {
-        state.session.avatarUrl = data
-      }
-    },
-    [types.LOGOUT](state) {
-      state.session = null
-      storage.remove('user_session')
+
+    actions: {
+        [types.LOAD_CURRENT_USER]({commit}, userId) {
+            let promise
+            if (userId) {
+                promise = axios.get(`/v1/userinfos/${userId}`)
+            } else {
+                promise = axios.get('/v1/userinfos/current')
+            }
+            return promise.then((res) => {
+                if (res.data.errcode === 0) {
+                    commit(types.CHANGE_SESSION, res.data.data)
+                }
+                return res.data.data
+            })
+        },
+        [types.GET_CURRENT_USER_INFO]({getters}) {
+            return new Promise((resolve) => {
+                if (getters.session) {
+                    resolve(getters.session)
+                } else {
+                    this.dispatch(types.LOAD_CURRENT_USER).then((userInfo) => {
+                        resolve(userInfo)
+                    })
+                }
+            })
+        },
+        [types.CHANGE_SESSION]({commit}, data) {
+            commit(types.CHANGE_SESSION, data)
+        },
+        [types.CHANGE_AVATAR]({commit}, data) {
+            commit(types.CHANGE_AVATAR, data)
+        },
+        [types.DELETE_USER_SESSION]({commit}) {
+            commit(types.CHANGE_SESSION, null)
+        },
+        [types.LOGOUT]({commit}) {
+            commit(types.LOGOUT)
+        },
+        [types.CHECK_USER_SESSION]({getters}) {
+            const authInfo = cookieStore.get('authInfo')
+            return new Promise((resolve) => {
+                if (!authInfo) {
+                    resolve(false)
+                } else {
+                    const jwt = authInfo.split('.')
+                    let userInfo = atob(jwt[1])
+                    try {
+                        userInfo = JSON.parse(userInfo)
+                    } catch (err) {
+                        console.error(err)
+                        userInfo = {}
+                    }
+
+                    // 没有登录态的，新登录的 或者已有登录态且跟当前登录态一致的，表示登录中
+                    resolve((!getters.session && userInfo.userId) || (getters.session && getters.session.userId === userInfo.userId))
+                }
+            })
+        },
+        [types.USER_LOGIN]({commit}, data) {
+            return axios.post('/v1/passport/login', data).then((res) => {
+                if (res.data.ret === 0 && res.data.errcode === 0) {
+                    commit(types.CHANGE_SESSION, res.data.data)
+                    return res.data.data
+                }
+                return Promise.reject(res.data.msg)
+            })
+        }
     }
-  },
-
-  actions: {
-    [types.LOAD_CURRENT_USER]({ commit }, userId) {
-      let promise
-      if (userId) {
-        promise = axios.get(`/v1/userinfos/${userId}`)
-      } else {
-        promise = axios.get('/v1/userinfos/current')
-      }
-      return promise.then((res) => {
-        if (res.data.errcode === 0) {
-          commit(types.CHANGE_SESSION, res.data.data)
-        }
-        return res.data.data
-      })
-    },
-    [types.GET_CURRENT_USER_INFO]({ getters }) {
-      return new Promise((resolve) => {
-        if (getters.session) {
-          resolve(getters.session)
-        } else {
-          this.dispatch(types.LOAD_CURRENT_USER).then((userInfo) => {
-            resolve(userInfo)
-          })
-        }
-      })
-    },
-    [types.CHANGE_SESSION]({ commit }, data) {
-      commit(types.CHANGE_SESSION, data)
-    },
-    [types.CHANGE_AVATAR]({ commit }, data) {
-      commit(types.CHANGE_AVATAR, data)
-    },
-    [types.DELETE_USER_SESSION]({ commit }) {
-      commit(types.CHANGE_SESSION, null)
-    },
-    [types.LOGOUT]({ commit }) {
-      commit(types.LOGOUT)
-    },
-    [types.CHECK_USER_SESSION]({ getters }) {
-      const authInfo = cookieStore.get('authInfo')
-      return new Promise((resolve) => {
-        if (!authInfo) {
-          resolve(false)
-        } else {
-          const jwt = authInfo.split('.')
-          let userInfo = atob(jwt[1])
-          try {
-            userInfo = JSON.parse(userInfo)
-          } catch (err) {
-            console.error(err)
-            userInfo = {}
-          }
-
-          // 没有登录态的，新登录的 或者已有登录态且跟当前登录态一致的，表示登录中
-          resolve((!getters.session && userInfo.userId) || (getters.session && getters.session.userId === userInfo.userId))
-        }
-      })
-    },
-    [types.USER_LOGIN]({ commit }, data) {
-      return axios.post('/v1/passport/login', data).then((res) => {
-        if (res.data.ret === 0 && res.data.errcode === 0) {
-          commit(types.CHANGE_SESSION, res.data.data)
-          return res.data.data
-        }
-        return Promise.reject(res.data.msg)
-      })
-    }
-  }
 }
 
 export default user
