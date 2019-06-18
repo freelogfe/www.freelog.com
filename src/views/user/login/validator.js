7d905bf09b8a7ed5028c143933aa458ab1e6ce8d
kai <kai@kaideMacBook-Pro.local>
2019-06-18 09:59:59 +0800

    Update validator.js

--- src/views/user/login/validator.js
+++ src/views/user/login/validator.js
@@ -1,6 +1,8 @@
 import i18n from '@/lib/i18n'
 
-export const validateLoginName = function (rule, value, callback) {
+export const EMAIL_REG = /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/
+export const PHONE_REG = /^1[34578]\d{9}$/
+export const validateLoginName = function(rule, value, callback) {
   if (value) {
     if (!EMAIL_REG.test(value) && !PHONE_REG.test(value)) {
       callback(new Error(i18n.t('login.validateErrors[0]')))
@@ -11,7 +13,5 @@ export const validateLoginName = function (rule, value, callback) {
     callback(new Error(i18n.t('login.validateErrors[1]')))
   }
 }
-
 export default validateLoginName
-export const EMAIL_REG = /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/
-export const PHONE_REG = /^1[34578]\d{9}$/
+
