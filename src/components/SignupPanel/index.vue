b65a39cf6c1ca5b4630e4a81423ad0e4e3322744
kai <kai@kaideMacBook-Pro.local>
2019-06-21 15:45:26 +0800

    注册提出

---
+++ src/components/SignupPanel/index.vue
@@ -0,0 +1,129 @@
+<template>
+    <section
+        class="login-section"
+        v-loading="logining"
+        :element-loading-text="$t('signup.loadingText')"
+    >
+        <header class="login-header">
+            <!--<h1 class="brand">-->
+            <!--<router-link to="/" tabindex="-1">freelog.com</router-link>-->
+            <!--</h1>-->
+            <el-alert
+                v-if="error"
+                :title="error.title"
+                type="error"
+                :description="error.message"
+                show-icon
+            />
+        </header>
+        <el-form
+            class="login-form"
+            auto-complete="off"
+            :model="model"
+            :rules="rules"
+            ref="signupForm"
+            label-width="80px"
+        >
+            <h2 class="heading">{{$t('signup.head')}}</h2>
+            <el-form-item
+                prop="loginName"
+                :label="$t('signup.loginName')"
+                required
+            >
+                <el-input
+                    type="text"
+                    v-model="model.loginName"
+                    :placeholder="$t('signup.loginNamePlaceholder')"
+                    autocomplete="off"
+                >
+                </el-input>
+            </el-form-item>
+            <el-form-item
+                prop="username"
+                :label="$t('signup.username')"
+                required
+            >
+                <el-input
+                    type="text"
+                    v-model="model.username"
+                    :placeholder="$t('signup.usernamePlaceholder')"
+                    autocomplete="off"
+                >
+                </el-input>
+            </el-form-item>
+            <el-form-item
+                prop="password"
+                :label="$t('signup.password')"
+                required
+            >
+                <el-input
+                    type="password"
+                    v-model="model.password"
+                    :placeholder="$t('signup.passwordPlaceholder')"
+                    autocomplete="new-password"
+                    :readonly="readonly"
+                ></el-input>
+            </el-form-item>
+            <el-form-item
+                prop="checkPassword"
+                :label="$t('signup.checkPassword')"
+                required
+            >
+                <el-input
+                    type="password"
+                    v-model="model.checkPassword"
+                    :placeholder="$t('signup.checkPasswordPlaceholder')"
+                    autocomplete="new-password"
+                    :readonly="readonly"
+                ></el-input>
+            </el-form-item>
+            <el-form-item
+                prop="authCode"
+                :label="$t('signup.verifyCode')"
+            >
+                <el-input
+                    v-model="model.authCode"
+                    style="width: 60%"
+                    :placeholder="$t('signup.verifyCodePlaceholder')"
+                ></el-input>
+                <div class="check-code-wrap">
+                    <el-button
+                        class="vcode-btn"
+                        @click="sendCheckCodeNotifyHandler"
+                        :disabled="disabledCheckCodeBtn"
+                    >
+                        {{vcodeBtnText}}
+                    </el-button>
+                </div>
+            </el-form-item>
+            <el-form-item>
+                <router-link
+                    to="/login"
+                    class="user-op">
+                    {{$t('signup.loginText')}}<i class="el-icon-d-arrow-right"></i>
+                </router-link>
+            </el-form-item>
+            <el-form-item class="login-btns">
+                <el-button
+                    type="primary"
+                    style="width: 100%;"
+                    :loading="loading"
+                    @click="submit('signupForm')"
+                >
+                    {{ loading ? $t('signup.signupingText') : $t('signup.signupedText') }}
+                </el-button>
+            </el-form-item>
+        </el-form>
+        <!--<footer class="login-footer">-->
+        <!--<a href="/">返回到 freelog.com</a>-->
+        <!--</footer>-->
+    </section>
+</template>
+
+<script>
+    export {default} from './index';
+</script>
+
+<style lang="less" scoped>
+    @import "index.less";
+</style>
---
+++ src/components/SignupPanel/validator.js
@@ -0,0 +1,17 @@
+import i18n from '@/lib/i18n'
+
+export const EMAIL_REG = /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/
+export const PHONE_REG = /^1[34578]\d{9}$/
+export const validateLoginName = function(rule, value, callback) {
+  if (value) {
+    if (!EMAIL_REG.test(value) && !PHONE_REG.test(value)) {
+      callback(new Error(i18n.t('login.validateErrors[0]')))
+    } else {
+      callback()
+    }
+  } else {
+    callback(new Error(i18n.t('login.validateErrors[1]')))
+  }
+}
+export default validateLoginName
+
